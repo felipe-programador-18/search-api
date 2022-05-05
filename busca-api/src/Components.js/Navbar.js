3e604509ca766963eddecff95c1840b9bff7cd71
Felipe <machadofelipe2016@outlook.com>
2022-05-05 14:17:43 -0300

    c7-trade url inside out to adding value outside function

--- busca-api/src/Components.js/Navbar.js
+++ busca-api/src/Components.js/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 const NavBar = () => {
+  //is very interesting!!
+  const Imglink = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
   return (
           <nav>
               <div>
                   <img className='navbar-img'
                   alt='pokiapi-log'
-                  src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+                  src={Imglink}
                   />
               </div>
           </nav>
