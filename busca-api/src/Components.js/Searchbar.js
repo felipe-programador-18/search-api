e86370dfd89cc1fe4f272101deeedd3afeec6cf2
Felipe <machadofelipe2016@outlook.com>
2022-05-05 15:20:01 -0300

    c8-create searchjs to make input

---
+++ busca-api/src/Components.js/Searchbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Searchbar = () => {
+    return(
+        <div>
+            <div>
+              <input placeholder='Buscar Pokemon' ></input>
+            </div>
+        </div>
+    )
+}
+
+export default Searchbar
