a41b0687f3395f7f24ecbe2a44b6bf8a2e995626
Felipe <machadofelipe2016@outlook.com>
2022-05-05 15:53:54 -0300

    c11- adding style of field search and buttons

--- busca-api/src/Components.js/Searchbar.js
+++ busca-api/src/Components.js/Searchbar.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Searchbar = () => {
+    const [search, setsearch]= useState('dito')
+    
+    const OnchangeHandling = (e) => {
+        setsearch(e.target.value)
+    }
+    const OnbuttonClikHandlig = () => {
+        console.log('name of poke', search)
+    }
+
     return(
         <div className='searchbar-container'>
             <div className='searchbar'>
-              <input placeholder='Buscar Pokemon' />
+              <input placeholder='Buscar Pokemon' onChange={OnchangeHandling} />
             </div>
+           <div className='search-btn'>
+             <button onClick={OnbuttonClikHandlig}>Buscar!</button>
+           </div>
         </div>
     )
 }
