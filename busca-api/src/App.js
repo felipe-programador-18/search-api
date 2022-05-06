64ef8854612dfecbf655575cbf5a2a9734a49935
Felipe <machadofelipe2016@outlook.com>
2022-05-06 12:55:08 -0300

    c14- adding some change about all code

--- busca-api/src/App.js
+++ busca-api/src/App.js
@@ -1,4 +1,5 @@
 import React,{useEffect,useState} from 'react'
+import { GetPokemo } from './api';
 import './App.css'
 import NavBar from './Components.js/Navbar'
 import  Pokedex  from './Components.js/Pokedex';
@@ -8,10 +9,21 @@ import Searchbar from './Components.js/Searchbar'
 
 
 function App() {
+  // one flag date and anoter pokemon!!
   const [loading, setloading] = useState(false)
-  
+  const [pokemons, setpokemon] = useState([])
+
+
   const fectchingPokec= async () => {
+     try {
       setloading(true)
+      const result = await GetPokemo();
+      setpokemon(result)
+      setloading(false)
+     } catch (error) {
+       console.log('fetchpoke error', error)
+     }  
+   
   }
    
   useEffect(() => {
@@ -24,7 +36,7 @@ return (
     <div>
       <NavBar/>
       <Searchbar />
-      <Pokedex/>
+      <Pokedex pokemons={pokemons} loading={loading} />
      
     </div>
   );
