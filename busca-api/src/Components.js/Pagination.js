3ee13190e61ebaf2d88211b63d637c183b0592ef
Felipe <machadofelipe2016@outlook.com>
2022-05-06 19:23:48 -0300

    c33-set up all pagination

---
+++ busca-api/src/Components.js/Pagination.js
@@ -0,0 +1,16 @@
+import React from "react"
+// create my pagination !!!
+const Pagination = (props) => {
+    const {page, totalPages, onLeftClick, OnRightClick } = props;
+    return ( 
+         <div className="pagination-container">
+          <button onClick={onLeftClick}>⬅</button>
+            <div> {page} de: {totalPages} </div>
+          <button onClick={OnRightClick}>➡</button>
+          
+          </div>
+    )
+}
+
+
+export default Pagination
