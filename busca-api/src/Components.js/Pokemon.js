cc931b71b3fe72a20760193e72f57b8e63a636c8
Felipe <machadofelipe2016@outlook.com>
2022-05-06 14:26:46 -0300

    c23- trade react fragment for div

--- busca-api/src/Components.js/Pokemon.js
+++ busca-api/src/Components.js/Pokemon.js
@@ -1,7 +1,10 @@
-import React,{useState, useEffect} from 'react'
-
-const Pokemon = () => {
-
+import React from 'react'
+// this way i am get all pokemon picture making method props
+const Pokemon = (props) => {
+  const {pokemon} = props
+  return(<div>
+    {pokemon.name}
+  </div>)
 }
 
 export default Pokemon
