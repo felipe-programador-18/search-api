997d5acf5a8388d1b3c96d70a6c37de78f300240
Felipe <machadofelipe2016@outlook.com>
2022-05-06 16:18:40 -0300

    c27-adding more code about pokemon

--- busca-api/src/Components.js/Pokemon.js
+++ busca-api/src/Components.js/Pokemon.js
@@ -2,8 +2,35 @@ import React from 'react'
 // this way i am get all pokemon picture making method props
 const Pokemon = (props) => {
   const {pokemon} = props
-  return(<div>
-    {pokemon.name}
+  const onHeartClick = () =>{
+     console.log('can favorite')
+  }
+  const heart =  "❤"
+  return(
+  <div className='card-pokemon'>
+       <div className='pokemon-image-container'>
+          <img alt={pokemon.name} src={pokemon.sprites.front_default} className='pokemon-img' />
+       </div>
+       
+       <div className='card-body'>
+          <div className='card-top'>
+              <h3> {pokemon.name}</h3>
+              <div>{pokemon.id}</div>
+          </div>  
+            
+          <div className='card-button'>
+            <div className='pokemon-type'>
+              {pokemon.types.map((type, index) => 
+              { return <div key={index}  className='pokemon-type-text' >
+                   {type.type.name}
+              </div>
+            
+                  })}
+             </div>   
+              
+              <button className='pokemon-heart-btn' onClick={onHeartClick}> {} </button>
+           </div>
+       </div>
   </div>)
 }
 
