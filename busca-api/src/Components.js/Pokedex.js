305f299f2068a5a9b5b07627fde7542ffacbb990
Felipe <machadofelipe2016@outlook.com>
2022-05-06 14:25:48 -0300

    c22- finnaly of make pokemon props

--- busca-api/src/Components.js/Pokedex.js
+++ busca-api/src/Components.js/Pokedex.js
@@ -3,8 +3,8 @@ import Pokemon from './Pokemon';
 //here i create map to adding below the other and also verify loading with all code
 
 const Pokedex = (props) => {
-    const {pokemons, loading} = props ;
-    console.log('verify pokemon',pokemons)
+    const {pokemons, loading} = props;
+    console.log('pokemons here', pokemons)
     return( <div>
         <div className='pokedex-header'>
         <h1>Pokedex</h1>
@@ -15,7 +15,7 @@ const Pokedex = (props) => {
         ( <div className='pokedex-grid' >
           {pokemons && pokemons.map((pokemon, index) => {
               return(
-                <Pokemon/>
+                <Pokemon key={index} pokemon={pokemon} />
               );
           })}
         </div> )
