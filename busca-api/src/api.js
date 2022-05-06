f7f89c1c08a1ddd182ca76c4f6965376cc03bf69
Felipe <machadofelipe2016@outlook.com>
2022-05-06 14:53:41 -0300

    c25- create conts to adding promises all

--- busca-api/src/api.js
+++ busca-api/src/api.js
@@ -8,9 +8,21 @@ export const searchPokemo = async (pokemon) => {
       console.log('error here',error)
     }
 }
+//this functions caught all pokemon and search with limit about search!!
 export const GetPokemo = async (limit= 50 ,offset=0) => {
     try {
         let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+        const response = await fetch(url)
+        return await response.json()
+    } catch (error) {
+      console.log('error here',error)
+    }
+}
+
+// this another functions went created to receive url of all data
+//practice about that!!
+export const GetPokemonData = async (url) => {
+    try {
         const response = await fetch(url)
         return await response.json()
     } catch (error) {
