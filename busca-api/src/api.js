54c3be5c97d75ba17441f1e4f66c4c64dd586ef8
Felipe <machadofelipe2016@outlook.com>
2022-05-06 14:00:36 -0300

    c20-trade Url for url

--- busca-api/src/api.js
+++ busca-api/src/api.js
@@ -1,8 +1,8 @@
 // here i create one function to receive async to manager loading all dates!!!
 export const searchPokemo = async (pokemon) => {
     try {
-        let Url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-        const response = await fetch(Url)
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        const response = await fetch(url)
         return await response.json()
     } catch (error) {
       console.log('error here',error)
@@ -10,8 +10,8 @@ export const searchPokemo = async (pokemon) => {
 }
 export const GetPokemo = async (limit= 50 ,offset=0) => {
     try {
-        let Url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-        const response = await fetch(Url)
+        let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+        const response = await fetch(url)
         return await response.json()
     } catch (error) {
       console.log('error here',error)
