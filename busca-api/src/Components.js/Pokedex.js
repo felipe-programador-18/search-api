e7f5a304148fd4206c1dfca122b80a9b124a09d0
Felipe <machadofelipe2016@outlook.com>
2022-05-06 12:55:35 -0300

    c15-adding some change about file pokedex code

--- busca-api/src/Components.js/Pokedex.js
+++ busca-api/src/Components.js/Pokedex.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const Pokedex = () => {
+const Pokedex = (props) => {
+    const {pokemons, loading} = props ;
     return( <div>
         <div className='pokedex-header'>
         <h1>Pokedex</h1>
           Paginação:
         </div>
+        {loading ?  (<div>Carregando, fera aqui....</div>
+        ) : 
+        ( <div className='pokedex-grid' >
+          {pokemons.map((pokemon, index) => {
+              return {<Pokemon></Pokemon>}
+          })}
+        </div> )
+        }
     </div>
     )
 }
