d82e3cf0bc8e15f81cef5b14193582c5f79f2a32
Felipe <machadofelipe2016@outlook.com>
2022-05-09 15:26:18 -0300

    c38-adding some date about pagination

--- busca-api/src/Components.js/Pagination.js
+++ busca-api/src/Components.js/Pagination.js
@@ -1,11 +1,11 @@
 import React from "react"
 // create my pagination !!!
 const Pagination = (props) => {
-    const {page, totalPages, onLeftClick, OnRightClick } = props;
+    const {page, totalPages, OnLeftClick, OnRightClick } = props;
     return ( 
          <div className="pagination-container">
-          <button onClick={onLeftClick}><div>⬅</div></button>
-            <div> {page} de: {totalPages} </div>
+          <button onClick={OnLeftClick}><div>⬅</div></button>
+            <div> {page} de {totalPages} </div>
           <button onClick={OnRightClick}><div>➡</div> </button>
           
           </div>
