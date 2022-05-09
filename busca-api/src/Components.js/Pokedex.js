1e94f438422009b8a04d08417f231b3b1d653887
Felipe <machadofelipe2016@outlook.com>
2022-05-09 15:27:02 -0300

    c39-adding state to adding inside out pokedex

--- busca-api/src/Components.js/Pokedex.js
+++ busca-api/src/Components.js/Pokedex.js
@@ -5,12 +5,26 @@ import Pokemon from './Pokemon'
 
 //here i create map to adding below the other and also verify loading with all code
 const Pokedex = (props) => {
-    const {pokemons, loading} = props;
-    console.log('pokemons here', pokemons)
+    const {pokemons, loading, page, totalPages} = props;
+    //console.log('pokemons here', pokemons)
+     
+    // create two functions to paginations!!
+     const OnLeftClick = () => {
+        console.log('volta')
+      }
+      
+      const OnRightClick = () => {
+        console.log('avanca')
+      }
+      
     return( <div>
         <div className='pokedex-header'>
         <h1>Pokedex</h1>
-         <Pagination/>
+         <Pagination 
+         page ={page+1}
+         totalPages={totalPages} 
+         onLeftClick={OnLeftClick}
+         OnRightClick={OnRightClick} />
         </div>
         {loading ?  (<div>Carregando, fera aqui....</div>
         ) : 
