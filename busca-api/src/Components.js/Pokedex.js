1ef1fcb04f6eda5390786720281fdaae5e801e33
Felipe <machadofelipe2016@outlook.com>
2022-05-09 14:57:45 -0300

    c35-adding pagination inside out pokedex

--- busca-api/src/Components.js/Pokedex.js
+++ busca-api/src/Components.js/Pokedex.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import Pokemon from './Pokemon';
-//here i create map to adding below the other and also verify loading with all code
+import Pagination from './Pagination';
+import Pokemon from './Pokemon'
 
+
+//here i create map to adding below the other and also verify loading with all code
 const Pokedex = (props) => {
     const {pokemons, loading} = props;
     console.log('pokemons here', pokemons)
     return( <div>
         <div className='pokedex-header'>
         <h1>Pokedex</h1>
-          Paginação:
+         <Pagination/>
         </div>
         {loading ?  (<div>Carregando, fera aqui....</div>
         ) : 
