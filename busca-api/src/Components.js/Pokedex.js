c9d5a564f47bf9ce607ac653bd3a6c9e5a5239b1
Felipe <machadofelipe2016@outlook.com>
2022-05-10 15:28:51 -0300

    c52-solve issues and see more code

--- busca-api/src/Components.js/Pokedex.js
+++ busca-api/src/Components.js/Pokedex.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import Pagination from './Pagination';
+import Pagination from './Pagination'
 import Pokemon from './Pokemon'
 
 
 //here i create map to adding below the other and also verify loading with all code
 const Pokedex = (props) => {
-    const {pokemons, loading, page, setpage, totalPages} = props;
+    const {pokemons, loading, page, setPage, totalPages} = props;
     //console.log('pokemons here', pokemons)
      
     // create two functions to paginations!!
      const OnLeftHandlingClick = () => {
         if(page > 0){
           //this verify to stay page!!
-          setpage(page -1)
+          setPage(page -1)
         }
       }
       
       const OnRightHandlingClick = () => {
         if(page+1 !== totalPages){
           //aqui se a pagina for diferente do numero total de pagina 46.. sempre passar a pagina 
-          setpage(page+1)
+          setPage(page+1)
         }
       }
       
